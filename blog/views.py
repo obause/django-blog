ce1f48e1a782b5f99a4e45745eb5687828e99428
Ole Bause <ole@bause.me>
2022-12-11 13:44:18 +0100

    added models and data

--- blog/views.py
+++ blog/views.py
@@ -1,72 +1,6 @@
-from datetime import date
+from django.shortcuts import render, get_object_or_404
 
-from django.shortcuts import render
-
-posts_content = [
-    {
-        "slug": "dummy-example-post",
-        "image": "mountains.jpg",
-        "author": "Ole Bause",
-        "date": date(2022, 12, 7),
-        "title": "Dummy Example Post",
-        "excerpt": "Dies ist die Beschreibung zu diesem Dummy Post, um das Design zu testen.",
-        "content": """
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium doloremque, mollitia voluptatem qui
-            voluptatum odio quae minus cumque et autem, nihil numquam ipsam aliquam molestiae, facere veritatis
-            reprehenderit pariatur! Qui!
-
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium doloremque, mollitia voluptatem qui
-            voluptatum odio quae minus cumque et autem, nihil numquam ipsam aliquam molestiae, facere veritatis
-            reprehenderit pariatur! Qui!
-
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium doloremque, mollitia voluptatem qui
-            voluptatum odio quae minus cumque et autem, nihil numquam ipsam aliquam molestiae, facere veritatis
-            reprehenderit pariatur! Qui!
-            """
-    },
-    {
-        "slug": "dummy-example-post2",
-        "image": "coding.jpg",
-        "author": "Ole Bause",
-        "date": date(2022, 12, 6),
-        "title": "Second Dummy Example Post",
-        "excerpt": "Dies ist die Beschreibung zu diesem zweiten Dummy Post, um das Design zu testen.",
-        "content": """
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium doloremque, mollitia voluptatem qui
-            voluptatum odio quae minus cumque et autem, nihil numquam ipsam aliquam molestiae, facere veritatis
-            reprehenderit pariatur! Qui!
-
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium doloremque, mollitia voluptatem qui
-            voluptatum odio quae minus cumque et autem, nihil numquam ipsam aliquam molestiae, facere veritatis
-            reprehenderit pariatur! Qui!
-
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium doloremque, mollitia voluptatem qui
-            voluptatum odio quae minus cumque et autem, nihil numquam ipsam aliquam molestiae, facere veritatis
-            reprehenderit pariatur! Qui!
-            """
-    },
-    {
-        "slug": "third-dummy-example-post",
-        "image": "woods.jpg",
-        "author": "Ole Bause",
-        "date": date(2022, 12, 5),
-        "title": "Third Dummy Example Post",
-        "excerpt": "Dies ist die Beschreibung zu diesem dritten Dummy Post, um das Design zu testen.",
-        "content": """
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium doloremque, mollitia voluptatem qui
-            voluptatum odio quae minus cumque et autem, nihil numquam ipsam aliquam molestiae, facere veritatis
-            reprehenderit pariatur! Qui!
-
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium doloremque, mollitia voluptatem qui
-            voluptatum odio quae minus cumque et autem, nihil numquam ipsam aliquam molestiae, facere veritatis
-            reprehenderit pariatur! Qui!
-
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium doloremque, mollitia voluptatem qui
-            voluptatum odio quae minus cumque et autem, nihil numquam ipsam aliquam molestiae, facere veritatis
-            reprehenderit pariatur! Qui!
-            """
-    }
-]
+from .models import Post
 
 
 def get_date(post):
@@ -75,21 +9,23 @@ def get_date(post):
 
 # Create your views here.
 def start_page(request):
-    sorted_posts = sorted(posts_content, key=get_date, reverse=True)
-    latest_posts = sorted_posts[:3]
+    latest_posts = Post.objects.all().order_by("-date")[:3]
     return render(request, "blog/index.html", {
         "posts": latest_posts
     })
 
 
 def posts(request):
+    all_posts = Post.objects.all().order_by("-date")
     return render(request, "blog/all_posts.html", {
-        "all_posts": posts_content
+        "all_posts": all_posts
     })
 
 
 def post_detail(request, slug):
-    post = next(post for post in posts_content if post['slug'] == slug)
+    post = get_object_or_404(Post, slug=slug)
+    # post = next(post for post in posts_content if post['slug'] == slug)
     return render(request, "blog/post-detail.html", {
-        "post": post
+        "post": post,
+        "tags": post.tags.all()
     })
